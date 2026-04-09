a2fca0956dca75f106e991f34fb337411a87088b
Eslam Hussien Sallam <e.sallam@mcsoil.com>
2026-04-09 13:31:40 +0200

    adding first phase

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from "./home/home";
+import { SiteHeader } from "./site-header/site-header";
+import { Catalog } from "./catalog/catalog";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, SiteHeader,Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/site-header/site-header.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bot-site-header',
+  imports: [],
+  templateUrl: './site-header.html',
+  styleUrl: './site-header.css',
+})
+export class SiteHeader {}
