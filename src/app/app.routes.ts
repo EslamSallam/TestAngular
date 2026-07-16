d195b81afbe91e454338c6bfb7ecfa540c6b7250
Eslam Hussien Sallam <e.sallam@mcsoil.com>
2026-07-16 17:50:48 +0300

    adding sign in component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Catalog } from './catalog/catalog';
 import { CartComponent } from './cart/cart.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
 
 export const routes: Routes = [
     {
@@ -12,6 +13,9 @@ export const routes: Routes = [
     },
     {
         path: 'cart',component: CartComponent
+    },
+    {
+        path: 'signIn',component: SignInComponent
     },
     {path :'',redirectTo: '/home',pathMatch:'full'}
 ];
--- src/app/user/sign-in/sign-in.component.ts
+++ src/app/user/sign-in/sign-in.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { IUserCredentials } from '../user.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'bot-sign-in',
+  imports: [FormsModule],
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent {
-
+  credentials: IUserCredentials = {email:'',password:''};
+  
   constructor() { }
+  SignIn(){
 
+  }
 }
